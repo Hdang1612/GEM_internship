d1a214c552660e113fb3c647d93b239e8225d804
Tô Hải Đăng <tohaidang161@gmail.com>
2025-06-19 17:23:56 +0700

    feat : responsive

---
+++ main.js
@@ -0,0 +1,21 @@
+const menu_icon = document.querySelector(".menu-icon");
+const menu_hidden = document.querySelector(".menu-hidden");
+const close_menu_btn = document.querySelector(".close-menu-btn");
+menu_icon.addEventListener("click", () => {
+  console.log("sdfasdfadfaf");
+  menu_hidden.style.display = "block";
+});
+
+close_menu_btn.addEventListener("click", () => {
+  console.log("sdfasdfadfaf");
+  menu_hidden.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (
+    !menu_hidden.contains(event.target) &&
+    !menu_icon.contains(event.target)
+  ) {
+    menu_hidden.style.display = "none";
+  }
+});
